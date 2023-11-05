06b9e12b608f849676b63710db48e9d4ee876acd
ccgsisig <87790430+ccgsisig@users.noreply.github.com>
2023-11-05 11:21:19 -0600

    genocide

--- src/variables/charts.js
+++ src/variables/charts.js
@@ -84,12 +84,20 @@ export const barChartOptionsDashboard = {
 
 export const lineChartDataDashboard = [
   {
-    name: "Mobile apps",
+    name: "Goldman Sachs",
     data: [500, 250, 300, 220, 500, 250, 300, 230, 300, 350, 250, 400],
   },
   {
-    name: "Websites",
+    name: "JP Morgan Chase & Co.",
     data: [200, 230, 300, 350, 370, 420, 550, 350, 400, 500, 330, 550],
+  },
+  {
+    name: "Blackrock",
+    data: [400, 230, 300, 350, 370, 420, 550, 350, 400, 500, 330, 550],
+  },
+  {
+    name: "Morgan Stanley",
+    data: [300, 230, 300, 350, 370, 420, 550, 350, 400, 500, 330, 550],
   },
 ];
 
